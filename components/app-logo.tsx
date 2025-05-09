da0a424fb06829ebe235c8c7f5e6e18b18fc6696
Harshitha-Macha <machaharshitha3@gmail.com>
2025-05-09 17:38:07 +0530

    UPDATED LOGO

--- components/app-logo.tsx
+++ components/app-logo.tsx
@@ -5,7 +5,7 @@ export function AppLogo({ className = "" }: { className?: string }) {
   return (
     <Link href="/dashboard" className={`flex items-center ${className}`}>
       <div className="relative h-8 w-8 mr-2">
-        <Image src="/logo.png" alt="Vaya Logo" fill className="object-contain" priority />
+        <Image src="/logo.jpg" alt="Vaya Logo" fill className="object-contain" priority />
       </div>
       <span className="font-bold text-lg">Vaya</span>
     </Link>
